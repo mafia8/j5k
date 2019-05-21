6b902beb0601f75219a9143bb9e61ea52f627863
mafia8 <mafia8@mail.ru>
2019-05-21 11:12:01 +0300

    Update 01_02_1.php

--- 01_02_1.php
+++ 01_02_1.php
@@ -1,5 +1,12 @@
 <?
 
+/*
+  попытался оформить как в заданиии.
+  выдает ошибку.
+  работающий ответ тут:
+  https://github.com/mafia8/j5k/blob/master/01_02_2.php
+  но там много чего не так, как в задании.
+*/
 namespace Pikabu93\Misc;
 
 use Core\Lib\{Test87 as helper};
